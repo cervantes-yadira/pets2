1537935346585cefc33124b520762cb7bc567545
Yadira Cervantes <cervantes.yadira@student.greenriver.edu>
2024-04-18 13:40:35 -0400

    Add all files from pets

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+// controller
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// require autoload file
+require_once('vendor/autoload.php');
+
+// instantiate F3 base class
+$f3 = Base::instance();
+
+// define a default route
+$f3->route('GET /', function() {
+//     echo '<h1>Pet Home</h1>';
+    // render a view page
+    $view = new Template();
+    echo $view->render('views/home.html');
+
+});
+
+// run fat-free
+$f3->run();
