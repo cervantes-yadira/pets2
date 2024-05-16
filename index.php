de2f479453bd56e3db2d7cedf1bdce7488e9df5a
Jennifer McNiel <jennifer.mcniel@yahoo.com>
2024-05-16 15:06:55 -0400

    added POST data from stuffed and robotic pet order to pet object

--- index.php
+++ index.php
@@ -48,20 +48,53 @@ $f3->route('GET|POST /order', function($f3) {
 
 });
 
-$f3->route('GET|POST /stuffed-order', function () {
+$f3->route('GET|POST /stuffed-order', function ($f3) {
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $size = $_POST['size'];
+        $stuffing = $_POST['stuffing'];
+        $material = $_POST['material'];
+
+        if(empty($size)) {
+            echo "Please supply a pet size";
+        } else if(empty($stuffing)) {
+            echo "Please supply a pet stuffing";
+        } else if(empty($material)){
+            echo "Please supply a pet material";
+        }else{
+            $f3->get('SESSION.pet1')->setSize($size);
+            $f3->get('SESSION.pet1')->setStuffing($stuffing);
+            $f3->get('SESSION.pet1')->setMaterial($material);
+
+            $f3->reroute('summary');
+        }
+    }
+
     $view = new Template();
     echo $view->render('views/stuffed-order.html');
 });
 
 
-$f3->route('GET|POST /robotic-order', function () {
+$f3->route('GET|POST /robotic-order', function ($f3) {
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+//        if (isset($_POST['accessories'])){
+//            $accessories = implode(" ", $_POST['accessories']);
+//        } else{
+            $accessories = $_POST['accessories'];
+//        }
+
+        $f3->get('SESSION.pet1')->setAccessories($accessories);
+
+        $f3->reroute('summary');
+    }
+
     $view = new Template();
     echo $view->render('views/robotic-order.html');
 });
 
-$f3->route('GET /summary', function() {
+$f3->route('GET /summary', function($f3) {
 //     echo '<h1>Pet Home</h1>';
     // render a view page
+    var_dump($f3->get('SESSION.pet1'));
     $view = new Template();
     echo $view->render('views/order-summary.html');
 });
